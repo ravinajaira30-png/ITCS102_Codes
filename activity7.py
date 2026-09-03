de17af18be1a1316975ef9c037679febf97c6f97
ravinajaira30-png <ravinajaira30@gmail.com>
2026-09-03 14:52:29 +0800

    Update activity7.py

--- activity7.py
+++ activity7.py
@@ -2,8 +2,20 @@
 
 total= 0
 
-n1= eval(input("input a number")
-total= total+n1
-  
-n2= float(input("input a number")
-total+=
+n1= eval(input("input a number -->"))
+total= total + n1
+n2= float(input("input a number -->"))
+total += n2
+n3= int(input("input a number -->"))
+total += n3
+n4= int(input("input a number -->"))
+total += n4
+n5= int(input("input a number -->"))
+total /= n5
+n6= int(input("input a number -->"))
+total -= n6
+n7= int(input("input a number -->"))
+total *= n7
+
+
+print("The total is --> ", total)
