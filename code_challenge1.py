ff04e83483397438530c2cb390535af1454ebbfd
ravinajaira30-png <ravinajaira30@gmail.com>
2026-09-03 14:32:33 +0800

    Add files via upload

---
+++ code_challenge1.py
@@ -0,0 +1,6 @@
+#asterisk 
+#code challenge 
+
+asterisk="\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t* \t\t*\n\t\t\t\t\t\t\t\t* \t\t\t\t*\n\t\t\t\t\t\t\t* \t\t\t\t\t\t*\n\t\t\t\t\t\t*\t\t\t my name is \t\t\t\t*\n \t\t\t\t\t*\t\t\t\t \"jaira ravina\"\t\t\t\t\t*\n\t\t\t\t\t\t* \t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t* \t\t\t\t*\n\t\t\t\t\t\t\t\t\t* \t\t*\n\t\t\t\t\t\t\t\t\t\t*"
+
+print(asterisk)
